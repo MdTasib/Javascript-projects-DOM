051368374a1b208154daef1ac3a742fa972634e4
MdTasib <ohidulalam442@gmail.com>
2022-02-14 12:46:37 +0600

    pin matcher done

--- pin-matcher-master/js/app.js
+++ pin-matcher-master/js/app.js
@@ -13,3 +13,33 @@ function generatePin() {
 	const pin = getPin();
 	document.getElementById("display-pin").value = pin;
 }
+
+document.getElementById("key-pad").addEventListener("click", function (event) {
+	const number = event.target.innerText;
+	const calcInput = document.getElementById("typed-numbers");
+
+	if (isNaN(number)) {
+		if (number == "C") {
+			calcInput.value = "";
+		}
+	} else {
+		const previosNumber = calcInput.value;
+		const newNumber = previosNumber + number;
+		calcInput.value = newNumber;
+	}
+});
+
+function submitVerify() {
+	const pin = document.getElementById("display-pin").value;
+	const typePin = document.getElementById("typed-numbers").value;
+	const succesMessage = document.getElementById("success");
+	const errorMessage = document.getElementById("error");
+
+	if (pin == typePin) {
+		succesMessage.style.display = "block";
+		errorMessage.style.display = "none";
+	} else {
+		succesMessage.style.display = "none";
+		errorMessage.style.display = "block";
+	}
+}
