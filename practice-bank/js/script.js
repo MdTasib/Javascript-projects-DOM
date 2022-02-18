55a215fe0cb35c4bd1289d66440bd4ec8383fac9
MdTasib <ohidulalam442@gmail.com>
2022-02-18 10:49:38 +0600

    bank functinal way to recap

--- practice-bank/js/script.js
+++ practice-bank/js/script.js
@@ -1,7 +1,25 @@
-document.getElementById("deposit-btn").addEventListener("click", function () {
-	const depositInput = document.getElementById("deposit-input");
-	const depositAmountText = depositInput.value;
-	const depositAmount = parseFloat(depositAmountText);
+function getInputValue(inputId) {
+	const input = document.getElementById(inputId);
+	const inputText = input.value;
+	const inputAmount = parseFloat(inputText);
+	input.value = "";
+	return inputAmount;
+}
 
-	depositInput.value = "";
+function calculateAmount(amountId, amount) {
+	const amountField = document.getElementById(amountId);
+	const amountFieldText = amountField.innerText;
+	const previosAmount = parseFloat(amountFieldText);
+	const newAmount = previosAmount + amount;
+	amountField.innerText = newAmount;
+}
+
+document.getElementById("deposit-btn").addEventListener("click", function () {
+	const amount = getInputValue("deposit-input");
+	calculateAmount("deposit-amount", amount);
+});
+
+document.getElementById("withdraw-btn").addEventListener("click", function () {
+	const amount = getInputValue("withdraw-input");
+	calculateAmount("withdraw-amount", amount);
 });
