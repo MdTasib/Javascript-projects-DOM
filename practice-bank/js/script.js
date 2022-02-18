7a03dee3be6c21547ae5e659482fce5d77300bc6
MdTasib <ohidulalam442@gmail.com>
2022-02-18 18:00:14 +0600

    functional way to bank project done

--- practice-bank/js/script.js
+++ practice-bank/js/script.js
@@ -14,12 +14,40 @@ function calculateAmount(amountId, amount) {
 	amountField.innerText = newAmount;
 }
 
+function updateTotal(amount, isAdding) {
+	const totalAmount = document.getElementById("total-amount");
+	const totalAmountText = totalAmount.innerText;
+	const previosAmount = parseFloat(totalAmountText);
+	let total;
+	if (isAdding) {
+		total = previosAmount + amount;
+	} else {
+		total = previosAmount - amount;
+	}
+
+	totalAmount.innerText = total;
+}
+
+function getInnerTextValue(fieldId) {
+	const fieldTag = document.getElementById(fieldId);
+	const fieldValue = fieldTag.innerText;
+	const value = parseFloat(fieldValue);
+	return value;
+}
+
 document.getElementById("deposit-btn").addEventListener("click", function () {
 	const amount = getInputValue("deposit-input");
-	calculateAmount("deposit-amount", amount);
+	if (amount > 0) {
+		calculateAmount("deposit-amount", amount);
+		updateTotal(amount, true);
+	}
 });
 
 document.getElementById("withdraw-btn").addEventListener("click", function () {
 	const amount = getInputValue("withdraw-input");
-	calculateAmount("withdraw-amount", amount);
+	const totalBalance = getInnerTextValue("total-amount");
+	if (amount > 0 && amount <= totalBalance) {
+		calculateAmount("withdraw-amount", amount);
+		updateTotal(amount, false);
+	}
 });
